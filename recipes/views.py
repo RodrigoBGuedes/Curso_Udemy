bd87db1d3f3d1c58a4916377492d93d1dc8ff0b4
RodrigoBGuedes <gedsao77@hotmail.com>
2024-06-26 14:35:10 -0300

    Finalizando aula 118

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,32 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
+from utils.pagination import make_pagination_range
 
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
-    ).order_by('-id')
+    ).order_by("-id")
 
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
+    try:
+        current_page = int(request.GET.get("page", 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 9)
+    page_object = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(paginator.page_range, 4, current_page)
+
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={"recipes": page_object, "pagination_range": pagination_range},
+    )
 
 
 def category(request, category_id):
@@ -20,39 +34,54 @@ def category(request, category_id):
         Recipe.objects.filter(
             category__id=category_id,
             is_published=True,
-        ).order_by('-id')
+        ).order_by("-id")
     )
 
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category | '
-    })
+    return render(
+        request,
+        "recipes/pages/category.html",
+        context={
+            "recipes": recipes,
+            "title": f"{recipes[0].category.name} - Category | ",
+        },
+    )
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+    recipe = get_object_or_404(
+        Recipe,
+        pk=id,
+        is_published=True,
+    )
 
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe,
-        'is_detail_page': True,
-    })
+    return render(
+        request,
+        "recipes/pages/recipe-view.html",
+        context={
+            "recipe": recipe,
+            "is_detail_page": True,
+        },
+    )
 
 
 def search(request):
-    search_term = request.GET.get("q", '').strip()
-    
+    search_term = request.GET.get("q", "").strip()
+
     if not search_term:
         raise Http404()
-    
+
     recipes = Recipe.objects.filter(
         Q(
-            Q(title__icontains=search_term) | 
-            Q(description__icontains=search_term),
+            Q(title__icontains=search_term) | Q(description__icontains=search_term),
         ),
-        is_published=True
-    ).order_by('-id')
-    
-    return render(request, 'recipes/pages/search.html', {
-        'page_title': f'Search for "{ search_term }" |',
-        'recipes': recipes,
-    })
+        is_published=True,
+    ).order_by("-id")
+
+    return render(
+        request,
+        "recipes/pages/search.html",
+        {
+            "page_title": f'Search for "{ search_term }" |',
+            "recipes": recipes,
+        },
+    )
