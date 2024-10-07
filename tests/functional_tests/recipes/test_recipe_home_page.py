0eb7050ea9532bd5418436faa2684428ef878b95
RodrigoBGuedes <desenvolvedor2@proxion.com.br>
2024-10-07 10:26:11 -0300

    Finalizando aula 171

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -9,12 +9,12 @@ from .base import RecipeBaseFunctionalTest
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
-        self.make_recipe_in_batch(qtd=20)
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.sleep()
         self.assertIn('No recipes found here 🥲', body.text)
 
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_search_input_and_find_correct_recipes(self):
         recipes = self.make_recipe_in_batch()
         
@@ -39,6 +39,28 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         self.assertIn(
             title_needed,
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
+
+        self.sleep()
+    
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê que tem uma paginação e clina na página 2
+        page_2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        page_2.click()
+
+        # Vê que tem mais 2 receitas na página 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')),
+            2
         )
 
         self.sleep()
